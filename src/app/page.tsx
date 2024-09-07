58da05c6500c5a415bf7b6441a398416d0df34e8
coderYL2337 <coderyl2337@outlook.com>
2024-09-07 13:47:59 -0400

    delete usestate

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { useState } from "react"
+// import { useState } from "react"
 import { NewToDoForm} from "./_components/new-todo-form"
 import { useQuery,useMutation } from "convex/react";
 import { api } from "../../convex/_generated/api";
